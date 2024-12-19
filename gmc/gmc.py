376e1089fe83cc1bd6c3b584375432d87ff0060c
holger krekel <holger@merlinux.eu>
2024-12-19 22:58:06 +0100

    fix test and improve assertions about group membership consistency

--- gmc/gmc.py
+++ gmc/gmc.py
@@ -58,8 +58,15 @@ class Relay:
             update_peer_from_incoming_message(peer, msg)
             print(f"    new {peer}")
 
-    def assert_group_consistency(self):
-        peers = list(x for x in self.peers.values() if x.id in x.members)
+    def assert_group_consistency(self, peers=None):
+        if peers is None:
+            peers = list(x for x in self.peers.values() if x.id in x.members)
+            # checking that actors do not contain peers who themselves think they are not members
+            left_peers = list(x for x in self.peers.values() if x.id not in x.members)
+            for peer in peers:
+                assert not peer.members.intersection(left_peers)
+
+        # checking that all peers have the same member list
         ok = True
         for peer1, peer2 in zip(peers, peers[1:]):
             if peer1.members == peer2.members:
--- gmc/test_immediate_consistency.py
+++ gmc/test_immediate_consistency.py
@@ -10,7 +10,9 @@ from gmc import (
 import pytest
 
 
-@pytest.mark.parametrize("n_actors,n_contacts,steps", [(5, 10, 50)])
+@pytest.mark.parametrize(
+    "n_actors,n_contacts,steps", [(2, 2, 20), (10, 20, 500), (20, 3, 500)]
+)
 def test_immediate_consistency(n_actors, n_contacts, steps):
     # Actors who are going to add/remove contacts
     # and send messages about this to each other.
@@ -45,6 +47,10 @@ def test_immediate_consistency(n_actors, n_contacts, steps):
             if possible_contacts:
                 DelMemberMessage(actor, member=random.choice(possible_contacts))
 
+    relay.receive_messages()
+    relay.assert_group_consistency(peers=list(actors))
+    # let one actor send a message to synchronize all contacts
+    ChatMessage(list(actors)[0])
     relay.receive_messages()
 
     relay.assert_group_consistency()
